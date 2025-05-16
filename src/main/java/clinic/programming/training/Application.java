da13b13362ae57c312653629c39b2f5d72902d17
Maria Angela Libre <Maria.Angela.Libre@ibm.com>
2025-05-16 13:59:00 +0800

    Commits

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,9 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords == null) ? 0: separateWords.length;
+	}
+
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
 greetings.add("Hello");
@@ -22,5 +28,9 @@ greetings.add("Hello");
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
+	int count = app.countWords("I have 4 words");
+	System.out.println("Word Count: " + count);
+
     }
 }
